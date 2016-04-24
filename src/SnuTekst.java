1da1c95d3339dd3c43ca124193cba3509e2ecc24
Dag Christian Nygaard <dag_christian_nygaard@hotmail.com>
2016-04-24 17:19:02 +0200

    Versjon 0.3

--- src/SnuTekst.java
+++ src/SnuTekst.java
@@ -1,10 +1,10 @@
 //Kilde: http://stackoverflow.com/questions/859562/whats-the-best-way-to-recursively-reverse-a-string-in-java
+//Del 3 av oppgaven allerede oppfylt?
 public class SnuTekst {
 	public static Integer antKall = 0;
 
 	SnuTekst() {
 	}
-
 	public static String SnuTekst(String tekst) {
 		antKall++;
 		int len = tekst.length();
